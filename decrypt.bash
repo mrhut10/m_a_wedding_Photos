633c40c73672604ccfefbc2716c90ee97edd35a0
mrhut10 <mrhut10@protonmail.com>
2019-02-18 22:06:51 +1100

    another fix to build scripts

--- decrypt.bash
+++ decrypt.bash
@@ -4,5 +4,6 @@ then
   echo "You forgot to specify the password"
   exit 1
 else
-  eval "mkdir -p build && cd build && gpg --pinentry-mode loopback --passphrase \"${FILES_PASS}\" -d \"..\files.tar.gz.gpg\" | tar xzvf -"
+  #eval "mkdir -p build && cd build && gpg --pinentry-mode loopback --passphrase \"${FILES_PASS}\" -d \"..\files.tar.gz.gpg\" | tar xzvf -"
+  eval "mkdir -p build && cd build && gpg --batch --passphrase \"${FILES_PASS}\" -d \"..\files.tar.gz.gpg\" | tar xzvf -"
 fi
--- encrypt.bash
+++ encrypt.bash
@@ -4,5 +4,6 @@ then
   echo "You forgot to specify the password"
   exit 1
 else
-  eval "cd raw && tar czvpf - * | gpg --pinentry-mode loopback --passphrase \"${FILES_PASS}\" --symmetric --cipher-algo aes256 -o ../files.tar.gz.gpg"
+  #eval "cd raw && tar czvpf - * | gpg --pinentry-mode loopback --passphrase \"${FILES_PASS}\" --symmetric --cipher-algo aes256 -o ../files.tar.gz.gpg"
+  eval "cd raw && tar czvpf - * | gpg --batch --passphrase \"${FILES_PASS}\" --symmetric --cipher-algo aes256 -o ../files.tar.gz.gpg"
 fi
